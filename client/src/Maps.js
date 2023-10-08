58f4a269166e51bf3805d6459def1795590c0e80
wayne-otido <otidowayne@gmail.com>
2023-10-09 01:48:43 +0300

    Fixed route not found

--- client/src/Maps.js
+++ client/src/Maps.js
@@ -32,7 +32,7 @@ function Maps(){
                     <h3 className="modeMapTitle">{mode.name} Maps:</h3>
                         {mode.maps.map((map)=>(
                             <div className="mapDivs">
-                                <img alt={map.name} src={map.image} style={{ width: "100%"}}/>
+                                <a href={map.image}><img alt={map.name} src={map.image} style={{ width: "100%"}}/></a>
                                 <div>
                                     <p>{map.name}, {map.location}</p>
                                 </div>
